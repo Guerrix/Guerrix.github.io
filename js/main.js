567528837f3808b2153e35bdc88011daa629ac8f
Axel Valdez <axel@paskola.com>
2018-01-16 20:06:15 -0700

    Add color illustrations, remove unused images, adjust colors.

--- js/main.js
+++ js/main.js
@@ -3,32 +3,15 @@ $(function(){
         e.preventDefault();
         if($('body').hasClass('lightmode')){
             $('body').removeClass('lightmode');
-            lightImgs();
             localStorage.setItem('jGuerraLightMode', 'FALSE');
         }else{
             $('body').addClass('lightmode');
-            darkImgs();
             localStorage.setItem('jGuerraLightMode', 'TRUE');
         }
     });
 
-    function darkImgs(){
-        $('.idevice').each(function(){
-            $(this).attr('src', $(this).attr('src').replace('.png', '-dark.png'));
-        });
-    }
-
-    function lightImgs(){
-        $('.idevice').each(function(){
-            $(this).attr('src', $(this).attr('src').replace('-dark.png', '.png'));
-        });
-    }
-
     var lightmode = localStorage.getItem('jGuerraLightMode');
     if(lightmode == 'TRUE'){
         $('body').addClass('lightmode');
-        darkImgs();
-    }else{
-        lightImgs();
     }
 });
